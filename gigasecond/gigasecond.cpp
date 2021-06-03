35166636ea84dc382f1392097759911c595b7a3e
AnaBordignon <anaclaudiabordignon@gmail.com>
2021-06-03 16:11:17 +0200

    Added Gigasecond for adding a gigasecond to a given time moment. Fifth day exercise.

---
+++ gigasecond/gigasecond.cpp
@@ -0,0 +1,9 @@
+#include "gigasecond.h"
+
+namespace gigasecond {
+
+	ptime advance(ptime input) {
+		return input + seconds(1000000000);
+	}
+
+}  // namespace gigasecond
---
+++ gigasecond/gigasecond.h
@@ -0,0 +1,13 @@
+#if !defined(GIGASECOND_H)
+#define GIGASECOND_H
+
+#include "boost/date_time/posix_time/posix_time.hpp"
+
+namespace gigasecond {
+
+	using namespace  boost::posix_time;
+
+	ptime advance(ptime input);
+}  // namespace gigasecond
+
+#endif // GIGASECOND_H
